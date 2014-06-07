d4252aa733fb107d413c5ff354e11a1b7dd6656a
David Parsons <pars0098@flinders.edu.au>
2014-06-07 13:46:31 +0930

    Added slide left code

--- tilt.c
+++ tilt.c
@@ -7,7 +7,16 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
-
+  int c = 0;
+  while (line[0]==0 || c < length) {
+	  //slide tiles left
+	  if(line[0]==0) {
+		  line[0]=line[1];
+		  line[1]=line[2];
+		  line[2]=line[3];
+		  line[3]=0;
+	  }
+  }
   // combine tiles as required
 
   return 0;
